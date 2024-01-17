740afebcf25f98b6deec467b14c29eb2349e2663
JuanMartinRivas <jmartinrivas14@gmail.com>
2024-01-17 15:46:23 -0300

    Setting up vite

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+// import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <main>
+      <div className="container">
+        <h1>Hello, World!</h1>
+        <div className="row">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+            perferendis esse aspernatur fugit maiores dolorum voluptatem nemo
+            incidunt, quis, placeat fugiat, modi culpa omnis soluta commodi
+            veritatis. Consequatur, itaque repellendus!</p>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam
+            ad recusandae quibusdam, quaerat reiciendis consequuntur temporibus
+            modi laborum odit nisi optio, ex debitis! Ullam itaque et voluptates
+            enim eligendi.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem, minus et non odit sapiente ipsa quos, aperiam
+            doloribus quisquam aliquam accusantium esse cumque debitis!
+            Accusamus
+            adipisci numquam alias delectus in.</p>
+        </div>
+      </div>
+    </main>
+    </>
+  )
+}
+
+export default App
